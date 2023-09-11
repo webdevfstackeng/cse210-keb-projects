f0536df7bb191d950ed6f910691a0f6c92140108
webdevfstackeng <u175797830@byui.edu>
2023-09-11 21:30:30 +0200

    Completed Prep1.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Prompt user for the first name.
+        Console.Write("What is your first name? ");
+
+        // Store the first name in variable firstName.
+        string firstName = Console.ReadLine();
+
+        // Prompt user for the last name.
+        Console.Write("What is your last name? ");
+
+        // Store the lasst name in variable lastName.
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine(); //Leave a space.
+
+        // Display the results on the console.
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        Console.WriteLine();
     }
 }
